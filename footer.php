e729eae82a6dba6fb03cfc191d12ed1fb7648436
Daniel Cantarín <omega_canta@yahoo.com>
2014-01-02 14:50:20 -0300

    Removed unwanted data.

--- footer.php
+++ footer.php
@@ -4,10 +4,7 @@
 				<footer id="footer">
 				<div class="footerLogos">
 					<div class="bodyFooter">
-					<h6>Av. Ing. Huergo 1481 - C1107APB | Ciudad Autónoma de Buenos Aires | Tel + 54 (011) 5295-2400</h6>
-					<a href="/certificaciones">
-					<img style="float:right; padding:6px 0px 0px;" title="Certificados" alt="Certificados" src="../lib/images/logo-certificados.gif"/>
-					</a>
+					<h6></h6>
 					</div>
 				</div>
 				</footer>
--- header.php
+++ header.php
@@ -7,7 +7,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>A N S V: Sistema de Carga de Siniestros</title>
+<title>Backend</title>
 <!--Scripts acá -->
 <script type="text/javascript" src="../lib/js/jquery-1.7.min.js" ></script>
 <script type="text/javascript" src="./js/app.lib.js" ></script>
@@ -28,11 +28,6 @@
 		<!--div class="bodySup"></div-->
 			<div class="bodyLogos">
 			<a href="/">
-				<img style="float:left" title="Logo ANSV" alt="ANSV" src="../lib/images/logo-ansv.gif">
-				</a>
-				<a target="_blank" href="http://www.mininterior.gov.ar/">
-				<img style="float:right" title="Logo Ministerio" alt="Ministerio del Interior" src="../lib/images/logo-ministerio.gif">
-				</a>
 				<div style="text-align:right;"><a href="./?form_operacion=logout">LOGOUT</a></div>
 			</div>
 			
